610484aaa3d69d30d5601d4d7c1a9b7a2896a969
Bruno da Cunha <brunodacunha@outlook.com>
2015-06-24 12:38:15 -0300

    Correção fase 13

--- resources/views/store/store.blade.php
+++ resources/views/store/store.blade.php
@@ -52,9 +52,9 @@
                     <div class="shop-menu pull-right">
                         <ul class="nav navbar-nav">
                             <li><a href="#"><i class="fa fa-user"></i> Minha conta</a></li>
-                            <li><a href="http://commerce.dev:10088/checkout"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-                            <li><a href="http://commerce.dev:10088/cart"><i class="fa fa-shopping-cart"></i> Carrinho</a></li>
-                            <li><a href="http://commerce.dev:10088/auth/login"><i class="fa fa-lock"></i> Login</a></li>
+                            <li><a href="{{ route('checkout_place') }}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                            <li><a href="{{ route('cart_index') }}"><i class="fa fa-shopping-cart"></i> Carrinho</a></li>
+                            <li><a href="/auth/login"><i class="fa fa-lock"></i> Login</a></li>
                         </ul>
                     </div>
                 </div>
